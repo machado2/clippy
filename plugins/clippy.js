fe8139ff8796cd03b2512908cf987e14d7cac37e
Fábio Machado de Oliveira <machado2@gmail.com>
2024-05-14 08:41:28 -0300

    update

--- plugins/clippy.js
+++ plugins/clippy.js
@@ -5,9 +5,9 @@
 
 const utils = require('../lib/utils');
 const debug = require('debug')('sockbot:plugins:summoner');
-const { Configuration, OpenAIApi } = require("openai");
-
-const openai = new OpenAIApi(new Configuration({ apiKey: process.env.OPENAI_KEY }));
+const { NodeHtmlMarkdown } = require('node-html-markdown');
+const fs = require('fs');
+const path = require('path');
 
 async function moderateContent(content) {
     const response = await openai.createModeration({ input: content });
@@ -18,43 +18,65 @@ async function moderateContent(content) {
     return !moderation_result;
 }
 
-async function generateContent(context, postContent, userName) {
-    const prompt = `
-You are Clippy, a parody of the Microsoft Office Assistant. Your answers should be funny and entertaining.
-Your answers will attempt to be funny, helpful and correct. If it's out of your depth, you'll try to be just
-funny and not necessarily correct. But you'll pretend to be correct and confident anyway.
+// system_message is initialized with the contents of the file ../system_message.txt
 
+const sysmsgpath = path.join(__dirname, 'system_message.txt');
+
+let system_message = fs.readFileSync(sysmsgpath, 'utf8');
+
+const system_message_complement = `
 You're talking in a forum, and your answers should follow the markdown format, without any links or images.
-Your answers can be long if the post you are replying warrants it. 
 All usernames are prefixed with an '@' character.
 
-You are talking to a user named @${userName}.
+Your username in the forum is 'clippy'.`;
 
-These are the latest posts in the topic:
+function format_system_message() {
+    return {
+        role: "system",
+        content: system_message + '\n' + system_message_complement
+    };
+}
 
-${context}
+async function generateContent(messages) {
+    const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
+            "X-Title": `Clippy`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "meta-llama/llama-3-70b-instruct",
+            // model: "openai/gpt-3.5-turbo",
+            // model: "mistralai/mixtral-8x22b",
+            temperature: 1,
+            messages,
+        })
+    });
+    const textData = await response.text();
+    console.log(textData);
+    const jsonData = JSON.parse(textData);
+    // const jsonData = await response.json();
+    console.log(JSON.stringify(jsonData));
+    const content = jsonData.choices[0].message?.content;
 
-This is the content of the post you're replying:
-
-${postContent}
-
-Clippy says:
-
-`;
-
-    if (!(await moderateContent(prompt))) {
-        return null;
+    // remove any trailing text in the format of <|xyz|>
+    if (content) {
+        return content.replace(/<\|.*\|>$/, '');
     }
 
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: prompt }],
-    });
-
-    const content = response.data.choices[0].message?.content;
     return content;
 }
 
+function convertHtmlToMarkdown(htmlString) {
+    // Create an instance of NodeHtmlMarkdown
+    const nhm = new NodeHtmlMarkdown();
+
+    // Convert the HTML string to Markdown
+    const markdown = nhm.translate(htmlString);
+
+    return markdown;
+}
 
 /**
  * Plugin generation function.
@@ -80,32 +102,60 @@ module.exports = function clippy(forum, config) {
         try {
             const user = await notification.getUser();
             debug(`clippy responding to ${user.username}`);
-    
+
             const thepost = await forum.Post.get(notification.postId);
             const topic = await forum.Topic.get(notification.topicId);
             let contextPosts = [];
             await topic.getLatestPosts(async (p) => {
                 const postAuthor = await forum.User.get(p.authorId);
-                contextPosts.push({content: p.content, author: postAuthor.username});
+                contextPosts.push({ content: p.content, author: postAuthor.username });
             });
-    
+
             contextPosts = contextPosts.slice(-100);
-            while (contextPosts.length > 1 && contextPosts.map((p) => p.content).join('\n\n').length > 8000) {
-                contextPosts.shift();
+            // while (contextPosts.length > 1 && contextPosts.map((p) => p.content).join('\n\n').length > 8000) {
+            // contextPosts.shift();
+            //}
+            let messages = [format_system_message()];
+            for (const p of contextPosts) {
+                // name must be ^[a-zA-Z0-9_-]{1,64}$
+                let sanitizedName = p.author.replace(/[^a-zA-Z0-9_-]/g, '').substring(0, 64);
+                if (sanitizedName.length == 0) {
+                    sanitizedName = 'user';
+                }
+                let role = sanitizedName.toLowerCase().trim() == 'clippy' ? 'assistant' : 'user';
+                messages.push({
+                    role,
+                    name: sanitizedName,
+                    content: convertHtmlToMarkdown(p.content)
+                });
+
+                // the command !clearcontext clear the context of the conversation
+                if (p.content.includes("!clearcontext")) {
+                    messages = [format_system_message()];
+                }
+
+                // the command !system_message(<new message>) changes the system message
+                const system_message_regex = /!system_message ?\((.*)\)/;
+                const match = p.content.match(system_message_regex);
+                if (match) {
+                    system_message = match[1];
+                    fs.writeFileSync(sysmsgpath, system_message);
+                    messages = [format_system_message()];
+                }
+
             }
-            const textLatestPosts = contextPosts.map((p) => `${p.author}: ${p.content}`).join('\n\n');
-            const response = await generateContent(textLatestPosts, thepost.content, user.username);
+            const response = await generateContent(messages);
             if (!response) {
                 return;
             }
-    
+
             return forum.Post.reply(notification.topicId, notification.postId, response);
         } catch (err) {
             forum.emit('error', err);
             return Promise.reject(err);
         };
     }
-    
+
 
     /**
      * Activate the plugin
@@ -127,4 +177,3 @@ module.exports = function clippy(forum, config) {
         handler: handler
     };
 };
-
