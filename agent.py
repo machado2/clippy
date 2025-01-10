290713299ec7796ba5346fa61952c0e69471b744
Fábio Machado de Oliveira <machado2@gmail.com>
2025-01-10 15:42:36 -0300

    Fixed history, I think

--- agent.py
+++ agent.py
@@ -1,10 +1,13 @@
+from typing import List
 from llama_index.tools.duckduckgo import DuckDuckGoSearchToolSpec
 from llama_index.core.agent import ReActAgent
 from llama_index.core.tools.function_tool import FunctionTool
 from llama_index.core.chat_engine.types import AgentChatResponse
 from image_generator import ImageGenerator
-from nodebb_lib import NodeBB
+from nodebb_lib import Comment, NodeBB
 from ask_image import AskImage
+from llama_index.core.chat_engine.types import ChatMessage
+from defaults import llm
 
 class Agent:
     def __init__(self, forum: NodeBB):
@@ -13,9 +16,12 @@ class Agent:
         tools = DuckDuckGoSearchToolSpec().to_tool_list()
         tools.append(FunctionTool.from_defaults(fn=self.image_generator.generate_and_upload_image))
         tools.append(FunctionTool.from_defaults(fn=self.ask_image.ask_image))
-        self.agent = ReActAgent.from_tools(tools, max_iterations=30)
+        self.agent = ReActAgent.from_tools(tools, max_iterations=30, llm=llm)
 
-    def chat(self, text: str) -> str:
+    def chat(self, history: List[ChatMessage], text: str) -> str:
+        self.agent.chat_history.clear()
+        self.agent.chat_history.extend(history)
+        print(self.agent.chat_history)
         answer: AgentChatResponse = self.agent.chat(text)
         return answer.response
 
--- clippy.py
+++ clippy.py
@@ -1,8 +1,17 @@
 import logging
 from agent import Agent
-from nodebb_lib import NodeBB, Notification
+from nodebb_lib import Comment, NodeBB, Notification
+from llama_index.core.chat_engine.types import ChatMessage
 
 logger = logging.getLogger(__name__)
+
+def _chatmessage(c: Comment) -> ChatMessage:
+    if c.user == "clippy":
+        # assistant image, dont include username
+        return ChatMessage(role="assistant", content=c.content)
+    else:
+        role = "assistant" if c.user == "clippy" else "user"
+        return ChatMessage(role="user", content=f"{c.user} said: {c.content}")
 
 class Clippy:
     def __init__(self, forum: NodeBB, agent: Agent):
@@ -14,36 +23,28 @@ class Clippy:
         topic = self.forum.get_topic(notification.tid)
         comments = self.forum.get_comments(topic)
 
+        system_message = ChatMessage(role="system", content=f"""
+            You are an assistant called clippy in the nodebb forum at https://what.thedailywtf.com.
+            If you want the user to be notified you replied or mentioned them you use an @ when writing their username,
+            like in @clippy would be used to mention you.
+
+            When asked to draw something, use a markdown image tag with the uploaded image URL so we can view it
+            directly in your posts. The image tag must be on it's own paragraph.
+                                     
+            You receive the users messages prefixed with "username said ...", but you reply with only
+            the markdown text of you reply post, in the way it should be posted on the forum.
+
+        """)
+        
         try:
             notif_comment = [c for c in comments if c.pid == notification.pid][0]
         except:
             logger.error("The comment referenced by the notification wasn't found")
             self.forum.mark_notification_read(notification.nid)
             return
-
-        comments_text = "\n\n".join([f"{c.user} said: {c.content}" for c in comments])
-        text_for_agent = f"""You are an assistant called clippy in the nodebb forum at https://what.thedailywtf.com.
-            If you want the user to be notified you replied or mentioned them you use an @ when writing their username,
-            like in @clippy would be used to mention you.
-
-            Use search whenever it makes sense to make a better answer.
-
-            Use the ask_image when understanding some image can help your answer.
-
-            When asked to draw something, use a markdown image tag with the uploaded image URL so we can view it
-            directly in your posts. The image tag must be on it's own paragraph.
-
-            Use as many tools calls as needed before replying.
-
-            You have been mentioned in the topic {topic.title}. These are the last comments from this topic:
-
-            {comments_text}
-            
-            You were mentioned in this comment, by {notification.username}:
-            
-            {notif_comment.content}
-            """
-        answer = self.agent.chat(text_for_agent)
+        history = [system_message] + [_chatmessage(c) for c in comments]
+        message = f"{notification.username} said: {notif_comment.content}"
+        answer = self.agent.chat(history, message)
         self.forum.reply_to_topic(topic.tid, notif_comment.pid, answer)
         self.forum.mark_notification_read(notification.nid)
         
