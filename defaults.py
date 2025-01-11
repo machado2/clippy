185e2eb3df9716171c39e08cb876a42f6c6fb209
Fábio Machado de Oliveira <machado2@gmail.com>
2025-01-11 00:36:53 -0300

    Moving to llama 3.3 and removing some dead code

--- defaults.py
+++ defaults.py
@@ -1,6 +1,5 @@
 from llama_index.llms.openrouter import OpenRouter
 import os
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-llm = OpenRouter(model="deepseek/deepseek-chat", api_key=os.environ["OPENROUTER_API_KEY"], max_tokens=16384)
-embed = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
+llm = OpenRouter(model="meta-llama/llama-3.3-70b-instruct", api_key=os.environ["OPENROUTER_API_KEY"], max_tokens=16384)
+
