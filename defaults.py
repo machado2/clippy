8fa2d0846d8654b1f9a4f8eb4a286bbddde8b306
Fábio Machado de Oliveira <machado2@gmail.com>
2025-01-11 00:40:55 -0300

    use environment to define the language model

--- defaults.py
+++ defaults.py
@@ -1,5 +1,8 @@
 from llama_index.llms.openrouter import OpenRouter
 import os
 
-llm = OpenRouter(model="meta-llama/llama-3.3-70b-instruct", api_key=os.environ["OPENROUTER_API_KEY"], max_tokens=16384)
 
+llm = OpenRouter(
+    model=os.environ["LANGUAGE_MODEL"] or "meta-llama/llama-3.3-70b-instruct",
+    api_key=os.environ["OPENROUTER_API_KEY"],
+    max_tokens=16384)
